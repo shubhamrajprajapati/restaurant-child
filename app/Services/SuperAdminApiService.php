9f513c0e98d6b53cac794164fb3aeaf7d3e80c59
Shubham Raj <shubhambth0000@gmail.com>
2024-10-24 11:38:45 +0530

    Refactor SuperAdminApiService to use app.url for RESTAURANT_URL fallback - Updated the constructor to use config('app.url') as the default value for appUrl if RESTAURANT_URL is not set in the .env - Removed commented-out caching logic from requestData method for clarity

--- app/Services/SuperAdminApiService.php
+++ app/Services/SuperAdminApiService.php
@@ -13,16 +13,12 @@ class SuperAdminApiService
     public function __construct()
     {
         $this->baseUrl = env('API_URL'); // Load from .env
-        $this->appUrl = env('RESTAURANT_URL', config('app.name')); // Load from env otherwise config
+        $this->appUrl = env('RESTAURANT_URL', config('app.url')); // Load from env otherwise config
     }
 
     public function requestData()
     {
-        // return Cache::remember('super_admin_data', 60, function () {
-            $appUrl = config('app.url');
-            $response = Http::get("{$this->baseUrl}/api/v1/settings/{$this->appUrl}");
-
-            return $response->json();
-        // });
+        $response = Http::get("{$this->baseUrl}/api/v1/settings/{$this->appUrl}");
+        return $response->json();
     }
 }
