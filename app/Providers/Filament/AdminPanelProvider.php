bc7eae24db5d144896a0842f22e9bbdce27e1310
Shubham Raj <shubhambth0000@gmail.com>
2024-11-14 13:53:01 +0530

    Set admin panel default theme mode to Dark. - Updated the default theme mode to ThemeMode::Dark for improved user experience in dark mode.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers\Filament;
 use App\CentralLogics\Helpers;
 use App\Models\Restaurant;
 use App\Services\SuperAdminApiService;
+use Filament\Enums\ThemeMode;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -79,6 +80,7 @@ class AdminPanelProvider extends PanelProvider
                 // '*/admin/posts/*',
             ])
             ->unsavedChangesAlerts()
-            ->sidebarCollapsibleOnDesktop();
+            ->sidebarCollapsibleOnDesktop()
+            ->defaultThemeMode(ThemeMode::Dark);
     }
 }
