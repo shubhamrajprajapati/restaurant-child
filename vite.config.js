a401db4e38dd36c9e304986b56b74635843b0b78
Shubham Raj <shubhambth0000@gmail.com>
2024-11-14 09:22:14 +0530

    Create custom Filament theme with Tailwind CSS and register in panel provider.

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/filament/admin/theme.css",
             ],
             refresh: true,
         }),
