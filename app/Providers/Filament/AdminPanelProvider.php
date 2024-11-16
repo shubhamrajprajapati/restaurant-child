fc547844d365319c62da4db51b847dcd071426fd
Shubham Raj <shubhambth0000@gmail.com>
2024-11-16 12:24:36 +0530

    Again customize Filament panel theme colors: danger, gray, info, primary, success, and warning. - Changer primary color Amber to Indigo.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -49,7 +49,12 @@ class AdminPanelProvider extends PanelProvider
             ->profile()
 
             ->colors([
-                'primary' => Color::Amber,
+                'danger' => Color::Rose,
+                'gray' => Color::Gray,
+                'info' => Color::Blue,
+                'primary' => Color::Indigo,
+                'success' => Color::Emerald,
+                'warning' => Color::Orange,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
